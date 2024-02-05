00d951e5089aa1b2693c1550e26ae292b1a9bd03
Uche <coolrest02@gmail.com>
2024-02-05 09:36:48 +0000

    added display message

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ function App() {
 
   const random = process.env.REACT_APP_TESTVAR
   console.log(random)
+  let myvalue;
+  if(random == "123456789")
+  {
+    myvalue = "The variable is readable"
+  }
+  else{
+    myvalue = "The variable is not readable"
+  }
   return (
     <>
       <Users />
       <p>{random}</p>
-
+      
+       <p>{myvalue}</p> 
+      
     </>
   );
 }
