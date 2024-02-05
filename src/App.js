259f79a4ea2f07c1da648a5dd51e6a7272d26460
Uche <coolrest02@gmail.com>
2024-02-05 09:20:16 +0000

    add console log

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Users from "./components/Users";
 function App() {
 
   const random = process.env.REACT_APP_TESTVAR
+  console.log(random)
   return (
     <>
       <Users />
       <p>{random}</p>
+
     </>
   );
 }
