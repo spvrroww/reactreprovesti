a75456d8d32c1f70889255d4bbdca366c00855e2
Uche <coolrest02@gmail.com>
2024-02-05 08:08:48 +0000

    added env file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Users from "./components/Users";
 
 function App() {
+
+  const random = process.env.REACT_APP_TESTVAR
   return (
     <>
       <Users />
+      <p>{random}</p>
     </>
   );
 }
